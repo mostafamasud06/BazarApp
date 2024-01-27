3d971224d4cddd8eaee59e21d5c295f916255f11
Shafi-06 <mostafamasud312@gmail.com>
2024-01-27 21:32:17 +0600

    Succesfully done my first navigation

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Login from './src/auth/login'; // Import the default export without curly braces
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Login/>
       <StatusBar style="auto" />
     </View>
   );
